75530061d1150e60a809117329a609c7c7d3a91a
abdulvakhid <buriyevabdulvohid055@gmail.com>
2022-12-19 11:51:00 +0500

    Add ticket page

--- src/pages/Tickets.js
+++ src/pages/Tickets.js
@@ -7,6 +7,8 @@ import img5 from '../assets/images/avatar/5.png'
 import img6 from '../assets/images/avatar/6.png'
 import img7 from '../assets/images/avatar/7.png'
 import img8 from '../assets/images/avatar/8.png'
+import {Sort, Filter, ArrowLeft, ArrowReft, Menus, SelectArrow} from "../assets/icons/fullIcons"
+import "./tickets.scss"
 
 export const Tickets = () => {
   const usertickets = [
@@ -83,7 +85,7 @@ export const Tickets = () => {
       priority_color:"#29CC97",
     },
     {
-      id: 6,
+      id: 7,
       ticket_imag: { img6 },
       ticket_name: 'Referral Bonus',
       ticket_date: "Updated 4 day ago",
@@ -95,7 +97,7 @@ export const Tickets = () => {
       priority_color:"#FEC400",
     },
     {
-      id: 6,
+      id: 8,
       ticket_imag: { img7 },
       ticket_name: 'How do I change my password?',
       ticket_date: "Updated 6 days ago",
@@ -108,8 +110,66 @@ export const Tickets = () => {
     }
   ]
   return (
-    <div>
+    <div className='box-ticket'>
       
+      <div className='tickets-header'>
+        <h3 className='allticket'>All tickets</h3>
+        <div className='filterSort'>
+            <div>
+             <img src={<Sort/>} alt="sorting" />
+             <span className='sorting'>Sort</span>
+            </div>
+            <div>
+             <img src={<Filter/>} alt="sorting" />
+             <span className='filtering'>Filter</span>
+            </div>
+        </div>
+      </div>
+      
+      <div className='tickets'>
+            <div className='ticket-top'>
+              <p className='first'>Ticket details</p>
+              <p className='second'>Customer name</p>
+              <p className='third'>Date</p>
+              <p className='four'>Priority</p>
+            </div>
+            <ul className='ticket-list'>
+              {
+              usertickets.map(item =>  <li className='ticketitem' key={item.id}>
+                <div className='ticket-detail'>
+                  <img className='ticket-img' src={item.ticket_imag} alt="image" />
+                   <div className='ticket-name'>
+                    <p className='style'>{item.ticket_name}</p>
+                    <p className='styleop'>{item.ticket_date}</p>
+                  </div>
+                </div>
+                
+                <div className='ticket-content'>
+                    <div className='customer-names'>
+                      <p className='style'>{item.cusrtem_name}</p>
+                      <span className='styleop'>{item.cusrtem_date}</span>
+                    </div>
+
+                    <div className='customer-dates'>
+                      <p className='style'>{item.date_date}</p>
+                      <p className='styleop'>{item.date_time}</p>
+                    </div>
+                    <span style={{backgroundColor:item.priority_color}} className='info-ticket'>{item.priority_text}</span>
+                    <img className='menu-icon' src={<Menus/>} alt="menu" />
+                </div>
+              </li>)
+              }
+            </ul>
+            <div className='tickets-footer'>
+              <p className='select-arrow'>Rows per page: <p className='countpage'>8 <img src={SelectArrow} alt=""/></p></p>
+              <span className='allpages'>1-8 of 1240</span>
+              <div>
+                <img src={ArrowLeft} alt="left" />
+                <img src={ArrowReft} alt="right" />
+              </div>
+            </div>
+      </div>
+
     </div>
   );
 };
